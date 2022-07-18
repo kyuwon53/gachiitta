f7c87d133a26faf07b7552280f0ac3ebbfc22481
kyuwon53 <lkw2v@naver.com>
2022-07-19 00:30:57 +0900

    [Add] JPA Auditing 활성화 애너테이션

--- src/main/java/com/gachi/itta/IttaApplication.java
+++ src/main/java/com/gachi/itta/IttaApplication.java
@@ -2,12 +2,14 @@ package com.gachi.itta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class IttaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IttaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(IttaApplication.class, args);
+    }
 
 }
