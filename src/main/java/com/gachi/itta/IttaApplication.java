3cf6a3b39162b2dc3a6ab6e54aa00965581dd4b4
kyuwon53 <lkw2v@naver.com>
2022-07-18 23:44:47 +0900

    Initial 프로젝트 초기 세팅

---
+++ src/main/java/com/gachi/itta/IttaApplication.java
@@ -0,0 +1,13 @@
+package com.gachi.itta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IttaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IttaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gachi/itta/IttaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachi.itta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IttaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
