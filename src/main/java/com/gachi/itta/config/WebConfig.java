1843d3a4fa65ab6171aa935a7e79cbf9eb112a17
kyuwon53 <lkw2v@naver.com>
2022-07-20 23:23:25 +0900

    LoginUserArgumentResolver 스프링에서 인식하도록 설정

---
+++ src/main/java/com/gachi/itta/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.gachi.itta.config;
+
+import com.gachi.itta.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
+    }
+}
